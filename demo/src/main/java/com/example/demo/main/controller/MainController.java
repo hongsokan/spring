d6e5fe7f02a47c6191591a18d777eeba8a59b288
hongsokan <hongsokan@daum.net>
2021-12-13 07:41:25 +0900

    form test

--- demo/src/main/java/com/example/demo/main/controller/MainController.java
+++ demo/src/main/java/com/example/demo/main/controller/MainController.java
@@ -4,12 +4,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.demo.main.dto.SampleDto;
+
 @Controller
 public class MainController {
 	
 	@RequestMapping("/")
-	public ModelAndView main(ModelAndView mav) {
+	public ModelAndView main(ModelAndView mav, SampleDto sample) {
+		mav.addObject("sample", sample);
 		mav.setViewName("main");
+		return mav;
+	}
+	
+	@RequestMapping("/ajax")
+	public ModelAndView ajax(ModelAndView mav, SampleDto sample) {
+		mav.addObject("sample", sample);
+		mav.setViewName("ajax");
+		return mav;
+	}
+	
+	@RequestMapping("/json")
+	public ModelAndView json(ModelAndView mav, SampleDto sample) {
+		mav.addObject("sample", sample);
+		mav.setViewName("json");
 		return mav;
 	}
 }
---
+++ demo/src/main/java/com/example/demo/main/dto/SampleDto.java
@@ -0,0 +1,15 @@
+package com.example.demo.main.dto;
+
+public class SampleDto {
+	private String text;
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+}
